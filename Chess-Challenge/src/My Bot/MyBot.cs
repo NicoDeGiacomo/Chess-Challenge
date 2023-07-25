0f67a9f8a4846dbe4cf6cb6b05e01e27e27c0039
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2023-07-25 15:11:16 -0300

    Added Alpha-Beta search.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using ChessChallenge.API;
-using ChessChallenge.Application;
 
 public class MyBot : IChessBot
 {
-    private const int MaxDepth = 4;
+    private const int MaxDepth = 3;
     private readonly int[] _pieceWeights = { 0, 100, 320, 330, 500, 900, 20000 };
     private Board _board;
 
@@ -23,16 +22,93 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         _board = board;
+
+        Move bestMove = new();
+        int bestScore = -int.MaxValue;
+        int alpha = -int.MaxValue;
+        int beta = int.MaxValue;
+
         Span<Move> moves = stackalloc Move[256];
         _board.GetLegalMovesNonAlloc(ref moves);
-        ConsoleHelper.Log("Evaluation: " + Evaluate());
-        return moves[0];
+        foreach (Move move in moves)
+        {
+            _board.MakeMove(move);
+            int score = -AlphaBeta(-beta, -alpha, MaxDepth - 1);
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestMove = move;
+            }
+
+            if (score > alpha)
+                alpha = score;
+            
+            _board.UndoMove(move);
+        }
+
+
+        // Null move safeguard
+        return bestMove == Move.NullMove ? moves[0] : bestMove;
+    }
+
+    private int AlphaBeta(int alpha, int beta, int depth)
+    {
+        // ConsoleHelper.Log("AlphaBeta - " + alpha + " " + beta + " " + depth);
+        int bestScore = -int.MaxValue;
+        if (depth == 0)
+            return Quiesce(alpha, beta);
+        
+        Span<Move> moves = stackalloc Move[256];
+        _board.GetLegalMovesNonAlloc(ref moves);
+        foreach (Move move in moves)
+        {
+            _board.MakeMove(move);
+            int score = -AlphaBeta(-beta, -alpha, depth - 1);
+            _board.UndoMove(move);
+
+            if (score >= beta)
+                return score;
+
+            if (score > bestScore)
+                bestScore = score;
+
+            if (score > alpha)
+                alpha = score;
+        }
+
+        return bestScore;
+    }
+
+    private int Quiesce(int alpha, int beta)
+    {
+        // ConsoleHelper.Log("Quiesce - " + alpha + " " + beta);
+        int standPat = Evaluate();
+        if (standPat >= beta)
+            return beta;
+        if (alpha < standPat)
+            alpha = standPat;
+
+        Span<Move> moves = stackalloc Move[256];
+        _board.GetLegalMovesNonAlloc(ref moves, true);
+        foreach (Move move in moves)
+        {
+            _board.MakeMove(move);
+            int score = -Quiesce(-beta, -alpha);
+            _board.UndoMove(move);
+
+            if (score >= beta)
+                return beta;
+            if (score > alpha)
+                alpha = score;
+        }
+
+        return alpha;
     }
 
     private int Evaluate()
     {
         if (_board.IsInCheckmate())
-            return (_board.IsWhiteToMove ? 1 : -1) * -1000000;
+            return (_board.IsWhiteToMove ? -20000 : 20000);
         if (_board.IsDraw())
             return 0;
 
@@ -58,8 +134,7 @@ public class MyBot : IChessBot
             blackPawnScore -= _pawnTable[63 - pawn.Square.Index];
         }
 
-        ConsoleHelper.Log("materialScore: " + materialScore + " whitePawnScore: " + whitePawnScore +
-                          " blackPawnScore: " + blackPawnScore);
-        return materialScore + whitePawnScore + blackPawnScore;
+        // ConsoleHelper.Log("Evaluation: " + (materialScore + whitePawnScore + blackPawnScore) * (_board.IsWhiteToMove ? 1 : -1));
+        return (materialScore + whitePawnScore + blackPawnScore) * (_board.IsWhiteToMove ? 1 : -1);
     }
 }
